256481a5a3db117346c466c1f386479f6c30bdc8
Inna <kostiuk.inna@gmail.com>
2020-07-04 16:20:19 +0900

    add search field to contact list

--- src/App.js
+++ src/App.js
@@ -1,32 +1,45 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
 
-const contacts = [
-  {
-    id: 'tyler',
-    name: 'Tyler McGinnis',
-    handle: '@tylermcginnis',
-    avatarURL: 'http://s.cdpn.io/3/kiwi.svg'
-  },
-  {
-    id: 'karen',
-    name: 'Karen Isgrigg',
-    handle: '@karen_isgrigg',
-    avatarURL: 'http://s.cdpn.io/3/kiwi.svg'
-  },
-  {
-    id: 'richard',
-    name: 'Richard Kalehoff',
-    handle: '@richardkalehoff',
-    avatarURL: 'http://s.cdpn.io/3/kiwi.svg'
-  },
-]
-
 class App extends Component {
+  state = {
+    contacts: [
+      {
+        id: 'tyler',
+        name: 'Tyler McGinnis',
+        handle: '@tylermcginnis',
+        avatarURL: 'http://s.cdpn.io/3/kiwi.svg'
+      },
+      {
+        id: 'karen',
+        name: 'Karen Isgrigg',
+        handle: '@karen_isgrigg',
+        avatarURL: 'http://s.cdpn.io/3/kiwi.svg'
+      },
+      {
+        id: 'richard',
+        name: 'Richard Kalehoff',
+        handle: '@richardkalehoff',
+        avatarURL: 'http://s.cdpn.io/3/kiwi.svg'
+      } 
+    ]
+  }
+
+  removeContact = contact => {
+    this.setState(currentState => ({
+      contacts: currentState.contacts.filter(c => {
+        return c.id !== contact.id
+      })
+    }))
+  }
+
   render() {
     return (
       <div>
-        <ListContacts contacts={contacts} />
+        <ListContacts 
+          contacts={this.state.contacts}
+          deleteContact={this.removeContact}
+        />
       </div>
     );
   }
--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,8 +1,34 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class ListContacts extends Component {
+    static propTypes = {
+        contacts: PropTypes.array.isRequired,
+        onDeleteContact: PropTypes.func.isRequired,
+    }
+    state = {
+        query: ''
+    }
+
+    updateQuery = (query) => {
+        this.setState(() => ({
+          query: query.trim()
+        }))
+      }
+    
     render() {
         return (
+            <div className='list-contacts'>
+                {JSON.stringify(this.state)}
+                <div className='list-contacts-top'>
+                    <input
+                        className='search-contacts'
+                        type='text'
+                        placeholder='Search Contacts'
+                        value={this.state.query}
+                        onChange={(event) => this.updateQuery(event.target.value)}    
+                    />
+                </div>
                 <ol className='contact-list'>
                     {this.props.contacts.map((contact) => (
                         <li key={contact.id} className="contact-list-item">
@@ -17,13 +43,17 @@ class ListContacts extends Component {
                                 <p>{contact.name}</p>
                                 <p>{contact.handle}</p>
                             </div>
-                            <button className='contact-remove'>
+                            <button 
+                                className='contact-remove'
+                                onClick={() => this.props.deleteContact(contact)}
+                                >
                                 Remove
                             </button>
                         </li>
                         )
                     )}
                 </ol>
+            </div>
         )
     }
 }
